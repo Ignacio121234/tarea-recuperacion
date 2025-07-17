0d7eb80d4faa6a75c531f633e8de7a8b6847e8c3
ignacio121234 <igarcia2020@udec.cl>
2025-07-17 16:58:04 -0400

    creacion clases ventana y PanelToDo

---
+++ src/main/java/org/example/Ventana.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame {
+    public Ventana() {
+
+        setTitle("APP");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 700);
+        setLayout(new BorderLayout());
+
+
+
+
+
+        setVisible(true);
+    }
+
+
+}
---
+++ src/main/java/org/example/paneToDo.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import javax.swing.*;
+
+public class paneToDo extends JFrame {
+
+}
