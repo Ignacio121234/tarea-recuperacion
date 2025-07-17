428b88395205987725231fe5eebc1ca6a04db1ec
ignacio121234 <igarcia2020@udec.cl>
2025-07-17 17:16:09 -0400

    creacion clases ventana y PanelToDo

--- src/main/java/org/example/paneToDo.java
+++ src/main/java/org/example/paneToDo.java
@@ -1,7 +1,13 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class paneToDo extends JFrame {
+public class paneToDo extends JPanel {
+
+    public paneToDo(){
+
+        setLayout(new FlowLayout());
+    }
 
 }
